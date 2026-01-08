86c64423279f1502b66dd04d8c53102a17c17a75
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-08 16:28:18 +0200

    Add styles for large and small text in index.tsx

---
+++ prodev-mobile-app-0x03/styles/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  textLarge: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: "#fff",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  textSmall: {
+    fontSize: 16,
+    color: "#fff",
+    textAlign: "center",
+  },
+});
