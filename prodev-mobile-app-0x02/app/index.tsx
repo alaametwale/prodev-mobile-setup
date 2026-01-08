afb97f6d65599bc6fa83115effcbb43d7d7d06ea
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-08 16:19:05 +0200

    Update index.tsx

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -7,46 +7,34 @@ import {
   Dimensions,
   TouchableOpacity,
 } from "react-native";
-import {
-  SafeAreaView,
-  SafeAreaProvider,
-} from "react-native-safe-area-context";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <ImageBackground
-          source={require("@/assets/images/background-image.png")}
+          source={require("../assets/images/background-image.png")}
           style={styles.background}
           resizeMode="cover"
         >
           <View style={styles.container}>
-            {/* Company Logo */}
             <View style={styles.companyLogo}>
-              <Image source={require("@/assets/images/Logo.png")} />
+              <Image source={require("../assets/images/Logo.png")} />
             </View>
 
-            {/* Text Group */}
             <View style={styles.textGroup}>
-              <Text style={styles.textLarge}>
-                Find your favorite place here
-              </Text>
-              <Text style={styles.textSmall}>
-                The best prices for over 2
-              </Text>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2 </Text>
               <Text style={styles.textSmall}>
                 million properties worldwide
               </Text>
             </View>
 
-            {/* Buttons & Navigation Prompt */}
             <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
               <View style={styles.buttonGroup}>
                 <TouchableOpacity style={styles.button}>
-                  <Text
-                    style={{ ...styles.textSmall, color: "black" }}
-                  >
+                  <Text style={{ ...styles.textSmall, color: "black" }}>
                     Join here
                   </Text>
                 </TouchableOpacity>
@@ -56,12 +44,8 @@ export default function Index() {
                 </TouchableOpacity>
               </View>
 
-              <View
-                style={{ alignItems: "center", paddingVertical: 20 }}
-              >
-                <Text style={{ color: "white" }}>
-                  Continue to home
-                </Text>
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
               </View>
             </View>
           </View>
@@ -110,7 +94,6 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 5,
     alignItems: "center",
-    fontSize: 20,
     flex: 1,
   },
   button: {
@@ -120,7 +103,6 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 5,
     alignItems: "center",
-    fontSize: 20,
     backgroundColor: "white",
     flex: 1,
   },
