70cda4748a075b9e29bd3e2bfa4fa58db5f526fb
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-08 16:32:35 +0200

    Update index.tsx

--- prodev-mobile-app-0x03/styles/index.tsx
+++ prodev-mobile-app-0x03/styles/index.tsx
@@ -1,16 +1,56 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({
-  textLarge: {
+const styles = StyleSheet.create({
+  navGroup: {
+    marginBottom: 30,
+    alignItems: "center",
+  },
+
+  largeText: {
     fontSize: 28,
     fontWeight: "700",
-    color: "#fff",
-    textAlign: "center",
     marginBottom: 10,
   },
-  textSmall: {
+
+  placeholderText: {
+    color: "#999",
+  },
+
+  formGroup: {
+    width: "100%",
+    marginBottom: 20,
+  },
+
+  inputField: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    padding: 15,
     fontSize: 16,
-    color: "#fff",
-    textAlign: "center",
+    marginBottom: 15,
+  },
+
+  passwordGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  forgotPasswordText: {
+    color: "#555",
+    textAlign: "right",
+    marginTop: 10,
+  },
+
+  socialMediaButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 12,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginTop: 10,
   },
 });
+
+export default styles;
